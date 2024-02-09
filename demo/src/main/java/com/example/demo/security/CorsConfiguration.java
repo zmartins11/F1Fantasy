b00ccf4fcc477108ff09dfb3e852d7222f9bd0de
zmartins11 <daniel98bm@gmail.com>
2024-02-09 00:47:42 +0000

    cors - spring deploy

--- demo/src/main/java/com/example/demo/security/CorsConfiguration.java
+++ demo/src/main/java/com/example/demo/security/CorsConfiguration.java
@@ -16,7 +16,9 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", "http://localhost:80", "http://localhost", "http://ec2-3-9-177-7.eu-west-2.compute.amazonaws.com")
+                        .allowedOrigins("http://localhost:4200", "http://localhost:80", "http://localhost",
+                                "http://ec2-3-9-177-7.eu-west-2.compute.amazonaws.com",
+                                "http://ec2-18-171-168-68.eu-west-2.compute.amazonaws.com")
                         .allowedMethods(HttpMethod.GET.name(),
                                 HttpMethod.POST.name(),
                                 HttpMethod.DELETE.name())
